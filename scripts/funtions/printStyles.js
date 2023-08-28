0f6529b7fd9962f5f2539cf1df9b42cd4e6ab02d
AinhoaAguado <ainhoa.aguado1@gmail.com>
2023-08-28 12:36:05 +0200

    add printStyles.js

---
+++ scripts/funtions/printStyles.js
@@ -0,0 +1,7 @@
+// Print Styles
+function printStyles (elementoTarea) {
+    elementoTarea.style.textDecoration = 'line-through';
+    elementoTarea.style.opacity = '0.5';
+    elementoTarea.style.color = 'green';
+  // agregar cambiar iconos??
+  }
