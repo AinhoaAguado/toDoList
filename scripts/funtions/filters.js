2c98be76818b48f4c159e79a25e29eb27e328069
AinhoaAguado <ainhoa.aguado1@gmail.com>
2023-08-28 12:36:50 +0200

    add filters.js

---
+++ scripts/funtions/filters.js
@@ -0,0 +1,7 @@
+//keyPressed
+window.addEventListener('keyup', event => {
+  let keyPressed = taskName.value;
+  console.log(event);
+  //console.log(taskName.value);
+  console.log(keyPressed);
+})
